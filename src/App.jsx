5ac39dd6d9c8a591e7b1e753a753507f426e586b
Abhishek Dhanke <abhishekrdhanke636@gmail.com>
2022-12-05 00:08:43 +0530

    feat : first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import QRCode from 'qrcode.react';
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [url,setUrl] = useState("");
+  
+  const handleDownload = () => {
+    const qrcode = document.getElementById("qrcode")
+    .toDataURL("image/png")
+    .replace("image/png", "image/octet-stream");
+
+    let elmnt = document.createElement("a");
+    elmnt.href = qrcode;
+    elmnt.download = "qrcode.jpg";
+    document.body.appendChild(elmnt);
+    elmnt.click();
+    document.body.removeChild(elmnt);
+  }
+  return (
+    <div className="App">
+
+<a href='https://github.com/abhishekrd/genQR' target="_blank" rel="noreferrer"><button className='github'><i class='bx bxl-github'></i> GitHub</button></a>
+      <h1 className='head'>genQR</h1>
+      <h2 className='subhead'>A QR Code Generator for your links🚀</h2>
+      <input className='input' type="text" placeholder="Enter your url here..." value={url} onChange={(e) => setUrl(e.target.value)} />
+    { url ? <QRCode id='qrcode' size={256} fgColor="#212121" value={url} includeMargin={true} renderAs="canvas" /> : <h2 className='info'>QR will be Generated here...</h2>}  
+     <button className='dwnldbtn' onClick={handleDownload} value="Download">Download</button>
+    </div>
+  )
+}
+
+export default App
